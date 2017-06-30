cfae824f0f773e1a9318ae4af8f18d3cd3eb089c
Alexeir7 <manchas1>
2017-06-29 19:52:47 -0600

    added prodile for users

--- imageboard/models.py
+++ imageboard/models.py
@@ -2,22 +2,29 @@ from django.db import models
 
 
 # Create your models here.
+class UserProfile(models.Model):
+    name = models.CharField(max_length=150, default=0)
+    email = models.EmailField(default='')
+    picture = models.ImageField(upload_to='profile_images', default='')
+
+
 class Post(models.Model):
-    post_title = models.CharField(max_length=50, default='')
-    post_image = models.ImageField(upload_to='post_images', default='')
-    post_stars = models.IntegerField(default=0)
-    post_views = models.IntegerField(default=0)
-    post_date = models.DateTimeField(auto_now_add=True)
+    id = models.ForeignKey(UserProfile, default='')
+    title = models.CharField(max_length=50, default='')
+    image = models.ImageField(upload_to='post_images', default='')
+    stars = models.IntegerField(default=0)
+    views = models.IntegerField(default=0)
+    date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.post_title
 
 
 class Comment(models.Model):
-    post_id = models.ForeignKey(Post, on_delete=models.CASCADE, default='')
-    comment_text = models.CharField(max_length=150, default='')
-    comment_stars = models.IntegerField(default=0)
-    comment_date = models.DateTimeField(auto_now_add=True)
+    id = models.ForeignKey(Post, on_delete=models.CASCADE, default='')
+    text = models.CharField(max_length=150, default='')
+    stars = models.IntegerField(default=0)
+    date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.comment_text
